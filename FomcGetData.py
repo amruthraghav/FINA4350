aa1a7933ce9948db78f18a9b11714bd4b3ece230
amruthraghav <amruth@hku.hk>
2020-12-01 12:35:34 +0800

    Added Calendar, Quandl File and the NLP Jupyter Notebook for

--- FomcGetData.py
+++ FomcGetData.py
@@ -3,12 +3,15 @@ import numpy as np
 import pandas as pd
 import pickle
 import sys
-
+from fomc_get_data.FomcStatement import FomcStatement
 from fomc_get_data.FomcMinutes import FomcMinutes
+from fomc_get_data.FomcMeetingScript import FomcMeetingScript
+from fomc_get_data.FomcPresConfScript import FomcPresConfScript
 from fomc_get_data.FomcSpeech import FomcSpeech
+from fomc_get_data.FomcTestimony import FomcTestimony
 
-def download_data(fomc, start_year):
-    df = fomc.get_contents(start_year)
+def download_data(fomc, from_year):
+    df = fomc.get_contents(from_year)
     print("Shape of the downloaded data: ", df.shape)
     print("The first 5 rows of the data: \n", df.head())
     print("The last 5 rows of the data: \n", df.tail())
@@ -16,23 +19,43 @@ def download_data(fomc, start_year):
     fomc.save_texts(prefix = fomc.content_type + "/FOMC_" + fomc.content_type + "_")
 
 if __name__ == '__main__':
-    
+    pg_name = sys.argv[0]
     args = sys.argv[1:]
+    content_type_all = ('all')
 
-    if len(args) == 0:
-        start_year = 1990
-    else:
-        start_year = int(args[0])
-    
-    if (start_year < 1980) or (start_year > 2020):
-        
-        print("Please specify the first argument as starting year between 1980 and 2020")
+    if (len(args) != 1) and (len(args) != 2):
+        print("Usage: ", pg_name)
+        print("Please specify the first argument from ", content_type_all)
+        print("You can add from_year (yyyy) as the second argument.")
+        print("\n You specified: ", ','.join(args))
         sys.exit(1)
 
-    print("Scraping: minutes")
-    fomc = FomcMinutes()
-    download_data(fomc, start_year)
+    if len(args) == 1:
+        from_year = 1990
+    else:
+        from_year = int(args[1])
+    
+    content_type = args[0].lower()
+    if content_type not in content_type_all:
+        print("Usage: ", pg_name)
+        print("Please specify the first argument from ", content_type_all)
+        sys.exit(1)
+    
+    if (from_year < 1980) or (from_year > 2020):
+        print("Usage: ", pg_name)
+        print("Please specify the second argument between 1980 and 2020")
+        sys.exit(1)
 
-    print("Scraping: speeches")
-    fomc = FomcSpeech()
-    download_data(fomc, start_year)
+    if content_type == 'all':
+        fomc = FomcStatement()
+        download_data(fomc, from_year)
+        fomc = FomcMinutes()
+        download_data(fomc, from_year)
+        fomc = FomcMeetingScript()
+        download_data(fomc, from_year)
+        fomc = FomcPresConfScript()
+        download_data(fomc, from_year)
+        fomc = FomcSpeech()
+        download_data(fomc, from_year)
+        fomc = FomcTestimony()
+        download_data(fomc, from_year)
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# FINA4350
-Group Project Work of Text Analytics and Natural Language Processing in Finance and Fintech
+# FINA4350 - Finding the Correlation of FED Speeches and Predicting the FED Fund Rate Decision using NLP and Sentiment Analysis
+
+
+## 1. Project Description
+Blog Post can be checked out here: https://financiallinguists.wordpress.com/
+
+Please refer to the post for a comprehensive understanding, project overview and analysis result.
+
+## 2. Create the directories
+    ```
+   cd data
+   mkdir FOMC MarketData LoughranMcDonald GloVe preprocessed models result
+   cd FOMC
+   mkdir statement minutes presconf_script meeting_script script_pdf speech testimony chair
+   cd ../MarketData
+    ```
+
+Please refer to the above link for Data with all the files: https://drive.google.com/drive/folders/10QyQ-nvP4x7hPWwX5JCOkxCf6-NJ_ViS?usp=sharing 
+
+## 3. Running the Files
+1. Get data from FOMC Website by specifying which year.
+   `python FomcGetData.py all 1980`
+2 . Get calendar from FOMC Website.
+   `python FomcGetCalendar.py 1980`
+3. Get data from Quandl. Specify the API Key and the date (yyyy-mm-dd).
+   `python QuandlGetData.py [the API Key] 1980-01-01`
+
+4. To Run Notebook
+    1. Go to top directory
+    `cd ../src`
+    2. Open and run notebooks
+    `jupyter notebook`
+    3. 
